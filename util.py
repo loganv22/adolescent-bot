972c5ac915e4d8cc85915221bd38e6769c8aa5ec
loganv22 <92587557+loganv22@users.noreply.github.com>
2022-02-19 15:45:39 -0500

    Update util.py

--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 import random
 
+# checks messages
 def containsAny(searchIn, strings): 
   searchIn = searchIn.lower()
   for i in strings:
     if i in searchIn: return i
   return None
 
+# turns file into list
 def textRead(fname):
   with open(fname) as f:
     fList = f.readlines()
@@ -13,6 +15,7 @@ def textRead(fname):
       fList[i] = fList[i].strip("\n")
   return fList
 
+# selects random joke from list
 def randomYourMomJoke(victim=""):
   insultList = textRead("insults/yourMom.txt")
 
@@ -21,12 +24,14 @@ def randomYourMomJoke(victim=""):
   else:
     return "Hey " + victim + ", " + random.choice(insultList)
 
+# creates a random joke that usually makes no sense
 def generateYourMomJoke(victim=""):
   verb = random.choice(textRead("insults/verbs.txt"))
   noun = random.choice(textRead("insults/nouns.txt"))
   adj = random.choice(textRead("insults/adjectives.txt"))
   joke_format = random.choice(textRead("insults/jokeTemplates.txt"))
 
+  # creates joke from random template and random words
   insult = joke_format.format(verb = verb, noun = noun, adjective = adj)
 
   if victim == "":
